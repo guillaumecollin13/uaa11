5719f591df22b1391d7fb99e00d97af52dee0145
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2023-01-24 12:03:20 +0100

    arbales

---
+++ tabCode/tabCode/Methodetraitement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabCode
+{
+    public struct Methodetraitement
+    {
+        public void generer(int nbval, int nbinter1, int nbinter2, out int[] tabSortie)
+        {
+            tabSortie=new int[nbval];
+            Random alea = new Random();
+            for (int compt = 0; compt <(nbval-1); compt++)
+            {
+                tabSortie[compt]= alea.Next(nbinter1, nbinter2);
+            }
+        }
+        public void tabMelange(string[] tMois,string[] tJour, out string[] t3)
+        {
+            t3 = new string[tJour.Length + tMois.Length];
+            for (int i = 0; i <(tJour.Length+tMois.Length); i=i+2)
+            {
+                t3[i] = tMois[i];
+            }
+            for (int i = 1; i < (tJour.Length + tMois.Length); i=i+2)
+            {
+                t3[i] = tJour[i];
+            }
+        }
+    }
+}
---
+++ tabCode/tabCode/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace tabCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbval;
+            int nbinter1;
+            int nbinter2;
+            int[] tabSortie;
+            int choix;
+            string recom;
+            string[] tMois = new string[12];
+            string[] tJour = new string[12];
+            string[] t3 = new string[tJour.Length+tMois.Length];
+            Methodetraitement taboutils = new Methodetraitement();
+
+
+            recom = "y";
+            do
+            {
+                Console.WriteLine("que voulez vous faire?");
+                Console.WriteLine("1 générer un tableau et l'afficher.");
+                Console.WriteLine("2 Mélanger deux tableaux(les jours et les mois) et afficher le résultat.");
+                Console.WriteLine("3 Générer un tableau d'entier et créer à partir de celui-ci un tableau contenant les valeurs paires et un autre avec les impaires. Les trois tableaux sont affichés.");
+                choix = int.Parse(Console.ReadLine());
+                if (choix == 1)
+                {
+                    Console.WriteLine("combien de valeur voulez vous ?");
+                    nbval = int.Parse(Console.ReadLine());
+                    Console.WriteLine("quelle est la valeur minimal de ces nopmbre");
+                    nbinter1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("quelle est la valeur minimal de ces nopmbre");
+                    nbinter2 = int.Parse(Console.ReadLine());
+
+                    taboutils.generer(nbval, nbinter1, nbinter2, out tabSortie);
+                    for (int compte = 0; compte < nbval - 1; compte++)
+                    {
+                        Console.Write(tabSortie[compte]);
+                    }
+                } else if (choix == 2)
+                {
+                    assign(tJour, tMois, out tJour, tMois);
+                    taboutils.tabMelange(tMois, tJour, out t3);
+                } else if (choix == 3)
+                {
+
+                }
+            } while (recom == "y");
+        }
+        static void assign(string[] tbJour, string[] tbMois,out string[] tJour, string[] tMois)
+        {
+            for (int i = 0; i < tbJour.Length; i++)
+            {
+                if (i == %2 )
+                {
+
+                }
+            }
+        }
+    }
+}
