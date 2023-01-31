087624fdd337dce4c93d7c9a8b2362043ed497dc
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2023-01-31 13:41:25 +0100

    uta

--- tabCode/tabCode/Methodetraitement.cs
+++ tabCode/tabCode/Methodetraitement.cs
@@ -17,15 +17,77 @@ namespace tabCode
         }
         public void tabMelange(string[] tMois,string[] tJour, out string[] t3)
         {
+            int i2 = 0;
             t3 = new string[tJour.Length + tMois.Length];
-            for (int i = 0; i <(tJour.Length+tMois.Length); i=i+2)
+            for (int i = 0; i <(tJour.Length+tMois.Length-1); i=i+2)
             {
-                t3[i] = tMois[i];
+                t3[i] = tMois[i2];
+                i2 = i2 + 1;
             }
+            i2 = 0;
             for (int i = 1; i < (tJour.Length + tMois.Length); i=i+2)
             {
-                t3[i] = tJour[i];
+                t3[i] = tJour[i2];
+                i2 = i2 + 1;
             }
+        }
+        public void tabClassPairImp(int[] tabEnt, out int[] tabPair, int[] tabImp)
+        {
+            int[] tabPairi = new int[tabEnt.Length];
+            int[] tabImpi = new int[tabEnt.Length];
+            int comptPair=0;
+            int comptImp=0;
+            for ( int i = 0;  i < tabEnt.Length; i++)
+            {
+                tabPairi[i] = 0;
+                tabImpi[i] = 0;
+            }
+            for (int pumpit = 0; pumpit < tabEnt.Length; pumpit++)
+            {
+                if (tabEnt[pumpit]%2==0)
+                {
+                    tabPairi[comptPair] = tabEnt[pumpit];
+                    comptPair = comptPair + 1;
+                    
+                }
+                else
+                {
+                    tabImpi[comptImp]= tabEnt[pumpit];
+                    comptImp = comptImp + 1;
+                }
+            }
+            tabPair = tabPairi;
+            tabImpi = tabImp;
+        }
+        public void tabDecal(int valInd, int[]tab ,out int[]tab1 )
+        {
+            int nbOcc = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] == valInd)
+                {
+                    nbOcc = nbOcc + 1;
+                }
+                else
+                {
+                    tab[i - nbOcc] = tab[i];
+                }
+            }
+            for (int i = tab.Length-nbOcc; i < tab.Length; i++)
+            {
+                tab[i] = valInd;       
+            }
+            tab1 = tab;
+        }
+        public void tabFibo(int nbval, int[]tabfibon1, out int []tabfibon)
+        {
+            tabfibon1[0] = 1;
+            tabfibon1[1] = 1;
+            for (int i = 2; i <nbval; i++)
+            {
+                tabfibon1[i] = tabfibon1[i - 1] + tabfibon1[i - 2];
+            }
+            tabfibon = tabfibon1;
         }
     }
 }
--- tabCode/tabCode/Program.cs
+++ tabCode/tabCode/Program.cs
@@ -12,20 +12,23 @@ namespace tabCode
             int[] tabSortie;
             int choix;
             string recom;
-            string[] tMois = new string[12];
-            string[] tJour = new string[12];
-            string[] t3 = new string[tJour.Length+tMois.Length];
+            string[] tMois = new string[12] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+            string[] tJour = new string[12] { "31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31" };
+            string[] t3 = new string[tJour.Length + tMois.Length];
+            int[] tabEnt = new int[20];
+            int[] tabPair = new int[tabEnt.Length];
+            int[] tabImp = new int[tabEnt.Length];
+            int valInd = 13;
+            int[] tab = new int[10] { 12, 9, 3, 13, 10, 13, 15, 13, 5, 16 };
             Methodetraitement taboutils = new Methodetraitement();
 
 
             recom = "y";
             do
             {
-                Console.WriteLine("que voulez vous faire?");
-                Console.WriteLine("1 générer un tableau et l'afficher.");
-                Console.WriteLine("2 Mélanger deux tableaux(les jours et les mois) et afficher le résultat.");
-                Console.WriteLine("3 Générer un tableau d'entier et créer à partir de celui-ci un tableau contenant les valeurs paires et un autre avec les impaires. Les trois tableaux sont affichés.");
-                choix = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                tryparse(out choix);
                 if (choix == 1)
                 {
                     Console.WriteLine("combien de valeur voulez vous ?");
@@ -40,25 +43,133 @@ namespace tabCode
                     {
                         Console.Write(tabSortie[compte]);
                     }
-                } else if (choix == 2)
-                {
-                    assign(tJour, tMois, out tJour, tMois);
-                    taboutils.tabMelange(tMois, tJour, out t3);
-                } else if (choix == 3)
-                {
-
+                    Console.WriteLine("");
                 }
+                else if (choix == 2)
+                {
+                    taboutils.tabMelange(tMois, tJour, out t3);
+                    for (int i = 0; i < tJour.Length; i++)
+                    {
+                        Console.Write(tJour[i] + " ");
+                    }
+                    Console.WriteLine("");
+                    for (int i = 0; i < tMois.Length; i++)
+                    {
+                        Console.Write(tMois[i] + " ");
+                    }
+                    Console.WriteLine("");
+                    for (int i = 0; i < t3.Length; i++)
+                    {
+                        Console.Write(t3[i] + ", ");
+                    }
+                    Console.WriteLine("");
+                }
+                else if (choix == 3)
+                {
+                    nbval = 20;
+                    nbinter1 = 1;
+                    nbinter2 = 100;
+                    taboutils.generer(nbval, nbinter1, nbinter2, out tabEnt);
+                    taboutils.tabClassPairImp(tabEnt, out tabPair, tabImp);
+                    for (int natsu = 0; natsu < tabEnt.Length; natsu++)
+                    {
+                        Console.Write(tabEnt[natsu] + " ");
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("voici les nombre paire");
+                    for (int mavis = 0; mavis < tabPair.Length; mavis++)
+                    {
+                        Console.Write(tabPair[mavis] + " ");
+                    }
+                    Console.WriteLine("");
+
+                    Console.WriteLine("voici les nombre impaire");
+                    for (int zeref = 0; zeref < tabImp.Length; zeref++)
+                    {
+                        Console.Write(tabImp[zeref] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+                if (choix == 4)
+                {
+                    Console.WriteLine("combien de terme voulez vous dans la suite de fibonacci?");
+                    nbval = int.Parse(Console.ReadLine());
+                    int[] tabfibon = new int[nbval];
+                    taboutils.tabFibo(nbval, tabfibon, out tabfibon);
+                    for (int i = 0; i < tabfibon.Length; i++)
+                    {
+                        Console.Write(tabfibon[i] + ",");
+                    }
+                    Console.WriteLine("");
+                }
+                else if (choix == 5)
+                {
+                    for (int i = 0; i < tab.Length; i++)
+                    {
+                        Console.Write(tab[i] + " ");
+                    }
+                    Console.WriteLine("");
+                    taboutils.tabDecal(valInd, tab, out tab);
+                    for (int i = 0; i < tab.Length; i++)
+                    {
+                        Console.Write(tab[i] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("vouolez-vous continuer si oui presser la touche y sin non presser n'importe quel touche");
+                recom = Console.ReadLine();
+
             } while (recom == "y");
         }
-        static void assign(string[] tbJour, string[] tbMois,out string[] tJour, string[] tMois)
+        static void tryparse(out int choix)
         {
-            for (int i = 0; i < tbJour.Length; i++)
+            bool trypasse;
+
+            Console.WriteLine("que voulez vous faire?");
+            Console.WriteLine("1 générer un tableau et l'afficher.");
+            Console.WriteLine("2 Mélanger deux tableaux(les jours et les mois) et afficher le résultat.");
+            Console.WriteLine("3 Générer un tableau d'entier et créer à partir de celui-ci un tableau contenant les valeurs paires et un autre avec les impaires. Les trois tableaux sont affichés.");
+            Console.WriteLine("4 faire un tableaux de fibonacci");
+            Console.WriteLine("5 range les occurrences d'une valeur indésirable à la fin d'un tableau");
+            do
             {
-                if (i == %2 )
+                
+                trypasse = false;
+
+                if (int.TryParse(Console.ReadLine(), out choix))
                 {
+                    if (choix<0 || choix>6)
+                    {
+
+                        Console.Clear();
+                      Console.WriteLine("ERROR ENTREE INCORRECT veuillez entrez une valeur correct");
+                        Console.WriteLine("que voulez vous faire?");
+                        Console.WriteLine("1 générer un tableau et l'afficher.");
+                        Console.WriteLine("2 Mélanger deux tableaux(les jours et les mois) et afficher le résultat.");
+                        Console.WriteLine("3 Générer un tableau d'entier et créer à partir de celui-ci un tableau contenant les valeurs paires et un autre avec les impaires. Les trois tableaux sont affichés.");
+                        Console.WriteLine("4 faire un tableaux de fibonacci");
+                        Console.WriteLine("5 range les occurrences d'une valeur indésirable à la fin d'un tableau");
+                    }
+                    else
+                    {
+                        trypasse = true;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("ERROR ENTREE INCORRECT veuillez entrez une valeur correct");
+                    Console.WriteLine("que voulez vous faire?");
+                    Console.WriteLine("1 générer un tableau et l'afficher.");
+                    Console.WriteLine("2 Mélanger deux tableaux(les jours et les mois) et afficher le résultat.");
+                    Console.WriteLine("3 Générer un tableau d'entier et créer à partir de celui-ci un tableau contenant les valeurs paires et un autre avec les impaires. Les trois tableaux sont affichés.");
+                    Console.WriteLine("4 faire un tableaux de fibonacci");
+                    Console.WriteLine("5 range les occurrences d'une valeur indésirable à la fin d'un tableau");
 
                 }
-            }
+            } while (trypasse == false);
+
         }
     }
 }
