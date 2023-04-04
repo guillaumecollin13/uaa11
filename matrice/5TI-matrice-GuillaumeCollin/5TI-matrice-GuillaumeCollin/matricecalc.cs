cbbf83e365c383add1c83b8833998f4b6828b104
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2023-04-04 12:00:27 +0200

    Neitrixy

--- matrice/5TI-matrice-GuillaumeCollin/5TI-matrice-GuillaumeCollin/matricecalc.cs
+++ matrice/5TI-matrice-GuillaumeCollin/5TI-matrice-GuillaumeCollin/matricecalc.cs
@@ -9,9 +9,9 @@ namespace _5TI_matrice_GuillaumeCollin
         public void remplissageMatrice(int [,] matri1 ,out int [,] matri)
         {
             Random alea = new Random();
-            for (int i = 0; i<matri1.GetLength(0)-1; i++)
+            for (int i = 0; i < matri1.GetLength(0); i++)
             {
-                for (int j = 0; j<matri1.GetLength(1)-1; j++)
+                for (int j = 0; j < matri1.GetLength(1); j++)
                 {
                     matri1[i, j]=alea.Next(0,20);
                 }
@@ -20,9 +20,9 @@ namespace _5TI_matrice_GuillaumeCollin
         public void concatMatrice(int[,] matri, out string matric)
         {
             matric = "";
-            for (int i = 0; i<matri.GetLength(0)-1; i++)
+            for (int i = 0; i<matri.GetLength(0); i++)
             {
-                for (int j = 0; j<matri.GetLength(1)-1; j++)
+                for (int j = 0; j<matri.GetLength(1); j++)
                 {
                     matric=matric+" "+matri[i,j];
                 }
@@ -35,9 +35,9 @@ namespace _5TI_matrice_GuillaumeCollin
             if (matri.GetLength(0) == matri2.GetLength(0) && matri2.GetLength(0) == matri.GetLength(0))
             {
                 possible = true;
-                for (int ligne = 0; ligne < matri2.GetLength(0)-1; ligne++)
+                for (int ligne = 0; ligne < matri2.GetLength(0); ligne++)
                 {
-                    for (int colone = 0; colone < matri.GetLength(1)-1; colone++)
+                    for (int colone = 0; colone < matri.GetLength(1); colone++)
                     {
                         matri3[ligne, colone] = matri[ligne, colone] + matri2[ligne, colone];
                     }
@@ -54,12 +54,12 @@ namespace _5TI_matrice_GuillaumeCollin
             if (matri.GetLength(0) == matri2.GetLength(1))
             {
                 possible = true;
-                for (int ligne = 0; ligne < matri2.GetLength(0) - 1; ligne++)
+                for (int ligne = 0; ligne < matri2.GetLength(0); ligne++)
                 {
-                    for (int colone = 0; colone < matri.GetLength(1) - 1; colone++)
+                    for (int colone = 0; colone < matri.GetLength(1); colone++)
                     {
                         matri3[ligne, colone] = 0;
-                        for (int multi = 0; multi < matri.GetLength(1)-1; multi++)
+                        for (int multi = 0; multi < matri.GetLength(1); multi++)
                         {
                             matri3[ligne, colone] = matri3[ligne, colone] + matri[ligne, colone] * matri2[ligne, colone];
                         }
